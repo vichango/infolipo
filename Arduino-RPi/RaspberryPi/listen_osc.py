fbd6211044e4c3d88621c5e587a05b9c9480c0ad
Vicho <vichango@gmail.com>
2022-12-10 18:15:30 +0100

    Print before sending.

--- Arduino-RPi/RaspberryPi/listen_osc.py
+++ Arduino-RPi/RaspberryPi/listen_osc.py
@@ -6,13 +6,16 @@ from pythonosc.osc_server import BlockingOSCUDPServer
 import serial
 import time
  
+# Sending.
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.flush() # Get rid of incomplete data.
 
+print("Getting ready.")
+
 def print_and_send(address, *args):
+    print(f"Will send /note {address}: {send_string}")
     send_string = f"{args}\n"
     ser.write(send_string.encode('utf-8'))
-    print(f"/note sent to {address}: {send_string}")
 
 def default_handler(address, *args):
     print(f"(unknown) not sent {address}: {args}")
