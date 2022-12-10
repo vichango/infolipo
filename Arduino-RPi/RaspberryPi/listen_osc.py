f01f7d09eb6d16f650d0580d5156119dd41e5ce4
Vicho <vichango@gmail.com>
2022-12-10 18:09:19 +0100

    Print before sending.

--- Arduino-RPi/RaspberryPi/listen_osc.py
+++ Arduino-RPi/RaspberryPi/listen_osc.py
@@ -7,25 +7,22 @@ import serial
 import time
  
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-# Get rid of incomplete data.
-ser.flush()
+ser.flush() # Get rid of incomplete data.
 
-def print_handler(address, *args):
-    print(f"{address}: {args}")
-
-def send_to_arduino(address, *args):
+def print_and_send(address, *args):
     send_string = f"{args}\n"
     ser.write(send_string.encode('utf-8'))
+    print(f"/note sent to {address}: {send_string}")
 
 def default_handler(address, *args):
-    print(f"DEFAULT {address}: {args}")
+    print(f"(unknown) not sent {address}: {args}")
 
 dispatcher = Dispatcher()
-dispatcher.map("/note*", send_to_arduino)
+dispatcher.map("/note*", print_and_send)
 dispatcher.set_default_handler(default_handler)
 
 ip = "192.168.0.255"
 port = 12000
 
 server = BlockingOSCUDPServer((ip, port), dispatcher)
-server.serve_forever()  # Blocks forever
+server.serve_forever() # Blocks forever
